dea17451186dbd1196452a1ae822b9716ba58bd7
Adam Scholey <hello@adamjs.net>
2020-04-12 00:21:25 +0100

    Establish DB connection

--- service/go.mod
+++ service/go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/LUSHDigital/core v0.23.1 // indirect
+	github.com/LUSHDigital/core-sql v0.4.2 // indirect
+	github.com/docker/go-metrics v0.0.1 // indirect
 	github.com/gorilla/mux v1.7.4 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 )
--- service/main.go
+++ service/main.go
@@ -6,19 +6,22 @@ import (
 	"net/http"
 	"os"
 
+	coresql "github.com/LUSHDigital/core-sql"
 	"github.com/LUSHDigital/core/rest"
-
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-
 	"github.com/joho/godotenv"
 )
 
 const (
 	envServicePort = "SERVICE_PORT"
+	envMySQLURL    = "MYSQL_URL"
 )
 
 func main() {
+	// setup
 	mustLoadEnv([]string{envServicePort})
+	db := coresql.MustOpen("mysql", os.Getenv(envMySQLURL))
 
 	r := mux.NewRouter()
 	r.HandleFunc("/{anything}", func(w http.ResponseWriter, r *http.Request) {
@@ -27,9 +30,27 @@ func main() {
 			rest.JSONError("vars went wrong").WriteTo(w)
 			return
 		}
+
+		rows, err := db.Query("SHOW DATABASES")
+		if err != nil {
+			rest.JSONError(err).WriteTo(w)
+			return
+		}
+
+		var results []string
+		var result string
+
+		for rows.Next() {
+			if err := rows.Scan(&result); err != nil {
+				rest.JSONError(err).WriteTo(w)
+				return
+			}
+			results = append(results, result)
+		}
+
 		rest.OKResponse(&rest.Data{
 			Type:    pathVal,
-			Content: "Hello World!",
+			Content: results,
 		}, nil).WriteTo(w)
 	})
 
