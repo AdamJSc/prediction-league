bdad4758a04b52a37975bec7f87e055627e1515c
Adam Scholey <hello@adamjs.net>
2020-04-13 23:48:57 +0100

    Truncate season created timestamp to nearest second when inserting

--- service/internal/domain/season_stmt.go
+++ service/internal/domain/season_stmt.go
@@ -9,7 +9,7 @@ func insertSeason(db coresql.Agent, s *Season) error {
 	stmt := `INSERT INTO season (id, year_ref, variant, name, entries_from, entries_until, start_date, end_date, created_at)
 					VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
-	now := time.Now()
+	now := time.Now().Truncate(time.Second)
 
 	if _, err := db.Query(
 		stmt,
