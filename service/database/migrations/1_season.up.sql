61826e2ef082572fb14c000de4106c8335d1b7e8
Adam Scholey <hello@adamjs.net>
2020-04-13 23:27:33 +0100

    Amend season migration

--- service/database/migrations/1_season.up.sql
+++ service/database/migrations/1_season.up.sql
@@ -1,10 +1,12 @@
 CREATE TABLE IF NOT EXISTS `season` (
     `id` VARCHAR(10) NOT NULL,
-    `name` VARCHAR(255),
+    `year_ref` INT NOT NULL,
+    `variant` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
     `entries_from` DATETIME NOT NULL,
     `entries_until` DATETIME,
-    `start_date` DATETIME,
-    `end_date` DATETIME,
+    `start_date` DATETIME NOT NULL,
+    `end_date` DATETIME NOT NULL,
     `created_at` DATETIME NOT NULL,
     `updated_at` DATETIME,
     PRIMARY KEY (id)
