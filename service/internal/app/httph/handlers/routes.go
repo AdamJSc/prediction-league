433cdeaf5db91532c8d9c5ceeb9e1a4b4ccb5b7c
Adam Scholey <hello@adamjs.net>
2020-04-19 21:45:47 +0100

    Remove create season handler and route

--- service/internal/app/httph/handlers/routes.go
+++ service/internal/app/httph/handlers/routes.go
@@ -7,13 +7,12 @@ import (
 
 // RegisterRoutes attaches all routes to the router
 func RegisterRoutes(c *httph.HTTPAppContainer) {
+	// placeholder endpoint
 	c.Router().HandleFunc("/{anything}", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world"))
 	}).Methods(http.MethodGet)
 
 	// api endpoints
 	api := c.Router().PathPrefix("/api").Subrouter()
-	api.HandleFunc("/season", createSeasonHandler(c)).Methods(http.MethodPost)
-
 	api.HandleFunc("/season/{season_id}/entry", createEntryHandler(c)).Methods(http.MethodPost)
 }
