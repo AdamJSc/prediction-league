a617513174e176ca90cdc045bfee28d364463094
Adam Scholey <hello@adamjs.net>
2020-04-30 22:21:17 +0100

    Install Vue and add a Vue component.
    Also replace Babel CLI usage with Webpack + Babel loader
    because native Babel wouldn't build Vue correctly...

---
+++ resources/src/js/components/HelloWorldComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>This is a message for {{ greetingName }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        props: {
+            greetingName: {
+                type: String
+            }
+        },
+        mounted: function() {
+            console.log(this.sayHello())
+        },
+        methods: {
+            sayHello: function () {
+                return "Hello " + this.greetingName + "!"
+            }
+        }
+    }
+</script>
--- resources/src/js/main.js
+++ resources/src/js/main.js
@@ -1,1 +1,9 @@
+import Vue from "vue/dist/vue.esm.js"
+
+Vue.component("hello-world", require("./components/HelloWorldComponent.vue").default)
+
+new Vue({
+    el: '#app'
+})
+
 console.log("compiled!")
