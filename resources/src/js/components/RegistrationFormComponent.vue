af5479f89c76819ce842034f38bd4e0e72c761e3
Adam Scholey <hello@adamjs.net>
2020-05-01 23:30:30 +0100

    Add Bootstrap and some basic styling

--- resources/src/js/components/RegistrationFormComponent.vue
+++ resources/src/js/components/RegistrationFormComponent.vue
@@ -1,25 +1,29 @@
 <template>
     <div class="form-container">
-        <form id="registration-form">
-            <div>
-                <div>Name</div>
-                <div><input type="text" name="name"/></div>
+        <form class="form-register">
+            <div class="form-label-group">
+                <input type="text" id="inputName" name="name" class="form-control" placeholder="Name" required autofocus>
+                <label for="inputName">Name</label>
             </div>
-            <div>
-                <div>Nickname</div>
-                <div><input type="text" name="nickname"/></div>
+
+            <div class="form-label-group">
+                <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email" required>
+                <label for="inputEmail">Email</label>
             </div>
-            <div>
-                <div>Email</div>
-                <div><input type="email" name="email"/></div>
+
+            <hr>
+
+            <div class="form-label-group">
+                <input type="text" id="inputNickname" class="form-control" placeholder="Nickname" required>
+                <label for="inputNickname">Nickname</label>
             </div>
-            <div>
-                <div>League PIN</div>
-                <div><input type="password" name="pin"/></div>
+
+            <div class="form-label-group">
+                <input type="password" id="inputPIN" class="form-control" placeholder="Password" required>
+                <label for="inputPIN">PIN</label>
             </div>
-            <div>
-                <div><button type="button" name="enter" v-on:click="submitForm()">Enter!</button></div>
-            </div>
+
+            <button class="btn btn-lg btn-primary btn-block" type="submit" v-on:click="submitRegistration">Enter</button>
         </form>
     </div>
 </template>
@@ -28,7 +32,9 @@
     export default {
         name: 'RegistrationForm',
         methods: {
-            submitForm: function () {
+            submitRegistration: function (e) {
+                e.preventDefault()
+                console.log(e)
                 console.log("form submission!")
             }
         }
