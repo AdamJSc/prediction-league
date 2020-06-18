5202b520abf8ee2c9cbbecec2a00b78c0a2b3aa2
Adam Scholey <hello@adamjs.net>
2020-06-18 21:28:44 +0100

    Update package-lock

--- resources/src/js/main.js
+++ resources/src/js/main.js
@@ -2,6 +2,7 @@ import Vue from "vue/dist/vue.esm.js"
 
 Vue.component("hello-world", require("./components/HelloWorldComponent.vue").default)
 
+// load components
 Vue.component("registration-workflow", require("./components/registration/RegistrationWorkflowComponent.vue").default)
 Vue.component("registration-form", require("./components/registration/RegistrationFormComponent.vue").default)
 Vue.component("registration-payment", require("./components/registration/RegistrationPaymentComponent.vue").default)
