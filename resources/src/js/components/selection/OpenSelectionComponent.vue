4e3b2a9480135e095487bf74aabded8bf7d772f1
Adam Scholey <hello@adamjs.net>
2020-06-20 17:38:16 +0100

    Handle change in state for submitting re-ordered team selection

--- resources/src/js/components/selection/OpenSelectionComponent.vue
+++ resources/src/js/components/selection/OpenSelectionComponent.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="col-md-8 offset-md-2">
         <div class="teams-container">
+            <p v-if="dirtyTeamIDs.length > 0">Changed {{dirtyTeamIDs.length}} team(s).</p>
             <p v-if="!isSelected()">Select a team to change their position.</p>
             <p v-else>Who do you want to swap {{getTeamByID(selectedTeamID).short_name}} with?</p>
             <table class="teams-table">
@@ -11,9 +12,17 @@
                     <td class="crest-outer"><div class="crest"><img :alt="getTeamByID(id).name" :src="getTeamByID(id).crest_url" /></div></td>
                     <td><span class="name">{{getTeamByID(id).short_name}}</span></td>
                 </tr>
+                <tr>
+                    <td colspan="3" class="submit-wrapper">
+                        <button v-on:click="updateOnClick" class="btn btn-primary" v-bind:disabled="dirtyTeamIDs.length === 0 || working">
+                            <span v-if="working">Working...</span>
+                            <span v-else>Update</span>
+                        </button>
+                        <button v-on:click="resetState" class="btn btn-secondary">Reset</button>
+                    </td>
+                </tr>
                 </tbody>
             </table>
-            <button class="btn btn-primary">Save</button>
         </div>
     </div>
 </template>
@@ -28,6 +37,7 @@
         },
         data: function() {
             return {
+                working: false,
                 teams: JSON.parse(this.teamsPayload),
                 teamsIDSequence: [],
                 selectedTeamID: null,
@@ -35,9 +45,7 @@
             }
         },
         mounted: function() {
-            for (let i = 0; i < this.teams.length; i++) {
-                this.teamsIDSequence.push(this.teams[i].id)
-            }
+            this.resetState()
         },
         methods: {
             getTeamByID: function(id) {
@@ -75,6 +83,23 @@
                     }
                 }
             },
+            resetState: function() {
+                this.teamsIDSequence = []
+                this.dirtyTeamIDs = []
+                this.selectedTeamID = null
+
+                for (let i = 0; i < this.teams.length; i++) {
+                    this.teamsIDSequence.push(this.teams[i].id)
+                }
+            },
+            storeReorderedTeams: function() {
+                let teams = []
+                for (let i = 0; i < this.teamsIDSequence.length; i++) {
+                    let id = this.teamsIDSequence[i]
+                    teams.push(this.getTeamByID(id))
+                }
+                this.teams = teams
+            },
             teamOnClick: function(e) {
                 let id = e.currentTarget.getAttribute('team-id')
                 switch (this.selectedTeamID) {
@@ -90,6 +115,11 @@
                 // swap this team with the already-selected team and reset selected team ID
                 this.swapTeams(this.selectedTeamID, id)
                 this.selectedTeamID = null
+            },
+            updateOnClick: function(e) {
+                this.working = true
+                this.storeReorderedTeams()
+                this.resetState()
             }
         },
     }
