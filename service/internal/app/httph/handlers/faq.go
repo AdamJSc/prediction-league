b5b943c096747167d7df94c8a7e17c99582e07cc
Adam Scholey <hello@adamjs.net>
2020-07-16 17:50:34 +0100

    Read and parse FAQs from file on a per-realm basis

--- service/internal/app/httph/handlers/faq.go
+++ service/internal/app/httph/handlers/faq.go
@@ -1,33 +1,29 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/gomarkdown/markdown"
+	"gopkg.in/yaml.v2"
 	"html/template"
+	"io/ioutil"
 	"prediction-league/service/internal/pages"
 )
 
 func getFAQPageData(realmName string) pages.FAQPageData {
 	var data pages.FAQPageData
 
-	faqs := []pages.FAQItem{
-		{
-			Question: "Which would you rather bee or a wasp?",
-			Answer: `That depends on what we mean by **bee**.
+	// read faqs for provided realm
+	contents, err := ioutil.ReadFile(fmt.Sprintf("./data/faq-by-realm/%s.yml", realmName))
+	if err != nil {
+		data.Err = err
+		return data
+	}
 
-In the traditional sense, a bee would be far more aware of its mortality following a single sting,
-so would logically cause _fewer_ injuries to [humans](https://en.wikipedia.org/wiki/Human).
-
-So, on the grounds of compassion, a bee.
-`,
-		},
-		{
-			Question: "What is the average air speed velocity of a laden swallow?",
-			Answer: `The real question is "_how did the coconut get to medieval England?_"`,
-		},
-		{
-			Question: "What's the Frequency, Kenneth?",
-			Answer: `"_Somewhere between 88.1 and 90.2 MHz on FM_" - **Mr Bruce**`,
-		},
+	// parse faqs
+	var faqs []pages.FAQItem
+	if err := yaml.Unmarshal(contents, &faqs); err != nil {
+		data.Err = err
+		return data
 	}
 
 	// markdownify the answers
