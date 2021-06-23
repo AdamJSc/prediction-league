f66b0f7810d354546bae2340f96b79bd9885dc90
Adam Scholey <hello@adamjs.net>
2021-06-23 06:59:27 +0100

    Update noop data source retrieval message

--- service/internal/domain/football_data_source.go
+++ service/internal/domain/football_data_source.go
@@ -18,7 +18,7 @@ type NoopFootballDataSource struct {
 // RetrieveLatestStandingsBySeason implements FootballDataSource
 func (l *NoopFootballDataSource) RetrieveLatestStandingsBySeason(_ context.Context, s Season) (Standings, error) {
 	l.l.Debugf("noop retrieved latest standings for season: %s", s.ID)
-	return Standings{}, errors.New("process aborted")
+	return Standings{}, errors.New("noop")
 }
 
 func NewNoopFootballDataSource(l Logger) (*NoopFootballDataSource, error) {
