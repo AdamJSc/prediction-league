b2b6054583ddbb2c66ef97269e7f27f0e2e3a003
Adam Scholey <hello@adamjs.net>
2021-07-28 23:28:15 +0100

    Update release script to include restarting of existing containers

--- release.sh
+++ release.sh
@@ -9,17 +9,24 @@ cd $1;
 # update shell script with current release data in case we need to manually restart the container later
 echo "#!/bin/sh
 
+# bring up any new containers
 RELEASE_TAG=$2 RELEASE_TIMESTAMP=\"$NOW\" docker-compose up -d --build;
+
+# restart existing containers
+RELEASE_TAG=$2 RELEASE_TIMESTAMP=\"$NOW\" docker-compose restart;
 
 if [ $? = 0 ]
 then
     echo "version $2 released successfully!";
     exit 0;
 else
-    echo "failed to release v$2";
+    echo "failed to release $2";
     exit 1;
 fi
 " > re-release.sh
+
+# make executable
+chmod 0755 ./re-release.sh
 
 # restart docker container with new image version
 /bin/sh ./re-release.sh
