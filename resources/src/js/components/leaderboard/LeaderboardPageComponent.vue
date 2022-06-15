cbce4649d42fbc4fcd5fee69bf30c9e334736e45
Adam Scholey <hello@adamjs.net>
2022-06-15 13:07:01 +0100

    style: update colour scheme + branding

--- resources/src/js/components/leaderboard/LeaderboardPageComponent.vue
+++ resources/src/js/components/leaderboard/LeaderboardPageComponent.vue
@@ -19,7 +19,7 @@
             ></scored-entry>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
           </div>
         </div>
       </div>
