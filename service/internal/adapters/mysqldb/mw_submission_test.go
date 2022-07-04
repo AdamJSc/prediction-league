b20276cfc77dcbd21165fb88c36249991848f60d
Adam Scholey <hello@adamjs.net>
2022-07-04 22:09:25 +0100

    test: add match week submission repo tests for insert, update and get by id methods

--- service/internal/adapters/mysqldb/mw_submission_test.go
+++ service/internal/adapters/mysqldb/mw_submission_test.go
@@ -45,6 +45,10 @@ var (
 
 	testDate time.Time
 	utc      *time.Location
+
+	uuidAll1s = `11111111-1111-1111-1111-111111111111`
+	uuidAll2s = `22222222-2222-2222-2222-222222222222`
+	uuidAll3s = `33333333-3333-3333-3333-333333333333`
 )
 
 func TestMain(m *testing.M) {
@@ -129,12 +133,44 @@ func TestNewMatchWeekSubmissionRepo(t *testing.T) {
 	})
 }
 
+func TestMatchWeekSubmissionRepo_GetByID(t *testing.T) {
+	t.Cleanup(truncate)
+
+	ctx := context.Background()
+
+	seedID := parseUUID(t, uuidAll1s)
+	seed := seedMatchWeekSubmission(t, generateMatchWeekSubmission(t, seedID, testDate))
+
+	repo, err := mysqldb.NewMatchWeekSubmissionRepo(db, nil, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("match week submission that exists must be returned successfully", func(t *testing.T) {
+		want := seed
+		got, err := repo.GetByID(ctx, seed.ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		cmpDiff(t, "match week submission", want, got)
+	})
+
+	t.Run("match week submission that does not exist must return the expected error", func(t *testing.T) {
+		nonExistentID := parseUUID(t, uuidAll2s)
+		_, err := repo.GetByID(ctx, nonExistentID)
+		if !errors.As(err, &domain.MissingDBRecordError{}) {
+			t.Fatalf("want missing db record error, got %+v (%T)", err, err)
+		}
+	})
+}
+
 func TestMatchWeekSubmissionRepo_GetByLegacyIDAndMatchWeekNumber(t *testing.T) {
 	t.Cleanup(truncate)
 
 	ctx := context.Background()
 
-	seedID := parseUUID(t, `11111111-1111-1111-1111-111111111111`)
+	seedID := parseUUID(t, uuidAll1s)
 	seed := seedMatchWeekSubmission(t, generateMatchWeekSubmission(t, seedID, testDate))
 
 	repo, err := mysqldb.NewMatchWeekSubmissionRepo(db, nil, nil)
@@ -153,7 +189,7 @@ func TestMatchWeekSubmissionRepo_GetByLegacyIDAndMatchWeekNumber(t *testing.T) {
 	})
 
 	t.Run("match week submission that does not exist by legacy id must return the expected error", func(t *testing.T) {
-		nonExistentID := parseUUID(t, `22222222-2222-2222-2222-222222222222`)
+		nonExistentID := parseUUID(t, uuidAll2s)
 		_, err := repo.GetByLegacyIDAndMatchWeekNumber(ctx, nonExistentID, seed.MatchWeekNumber)
 		if !errors.As(err, &domain.MissingDBRecordError{}) {
 			t.Fatalf("want missing db record error, got %+v (%T)", err, err)
@@ -170,13 +206,142 @@ func TestMatchWeekSubmissionRepo_GetByLegacyIDAndMatchWeekNumber(t *testing.T) {
 }
 
 func TestMatchWeekSubmissionRepo_Insert(t *testing.T) {
-	t.Skip()
-	// TODO: feat - write tests
+	t.Cleanup(truncate)
+
+	ctx := context.Background()
+
+	t.Run("passing nil match week submission must generate no error", func(t *testing.T) {
+		repo, err := mysqldb.NewMatchWeekSubmissionRepo(db, nil, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := repo.Insert(ctx, nil); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("valid match week submission must be inserted successfully", func(t *testing.T) {
+		submission := generateMatchWeekSubmission(t, uuid.UUID{}, time.Time{}) // empty id and createdAt timestamp
+		initialSubmission := cloneMatchWeekSubmission(submission)              // capture state before insert
+
+		insertID := parseUUID(t, uuidAll1s)
+		createdAt := testDate
+		repo, err := mysqldb.NewMatchWeekSubmissionRepo(db, newUUIDFunc(insertID), newTimeFunc(createdAt))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := repo.Insert(ctx, submission); err != nil {
+			t.Fatal(err)
+		}
+
+		want := initialSubmission
+		want.ID = insertID         // should be overridden on insert
+		want.CreatedAt = createdAt // should be overridden on insert
+
+		got, err := repo.GetByID(ctx, insertID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		cmpDiff(t, "match week submission", want, got)
+
+		// inserting same submission again must return the expected error
+		wantErrType := domain.DuplicateDBRecordError{}
+		gotErr := repo.Insert(ctx, got)
+		if !errors.As(gotErr, &wantErrType) {
+			t.Fatalf("want error (%T), got (%T)", wantErrType, gotErr)
+		}
+	})
+
+	t.Run("error generating uuid must return the expected error", func(t *testing.T) {
+		uuidFn := func() (uuid.UUID, error) {
+			return uuid.UUID{}, errors.New("sad times :'(")
+		}
+
+		repo, err := mysqldb.NewMatchWeekSubmissionRepo(db, uuidFn, newTimeFunc(testDate))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		submission := generateMatchWeekSubmission(t, uuid.UUID{}, time.Time{})
+
+		wantErrMsg := "cannot get uuid: sad times :'("
+		gotErr := repo.Insert(ctx, submission)
+		cmpErrorMsg(t, wantErrMsg, gotErr)
+	})
 }
 
 func TestMatchWeekSubmissionRepo_Update(t *testing.T) {
-	t.Skip()
-	// TODO: feat - write tests
+	t.Cleanup(truncate)
+
+	ctx := context.Background()
+
+	t.Run("passing nil match week submission must generate no error", func(t *testing.T) {
+		repo, err := mysqldb.NewMatchWeekSubmissionRepo(db, nil, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := repo.Update(ctx, nil); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("updating match week submission that exists must be successful", func(t *testing.T) {
+		createdAt := testDate
+
+		submission1ID := parseUUID(t, uuidAll1s)
+		seed1 := seedMatchWeekSubmission(t, generateMatchWeekSubmission(t, submission1ID, createdAt))
+
+		submission2ID := parseUUID(t, uuidAll2s)
+		seed2 := seedMatchWeekSubmission(t, generateMatchWeekSubmission(t, submission2ID, createdAt))
+
+		updatedAt := createdAt.Add(time.Second)
+		repo, err := mysqldb.NewMatchWeekSubmissionRepo(db, nil, newTimeFunc(updatedAt))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		changedSeed1 := &domain.MatchWeekSubmission{
+			ID:                      seed1.ID,
+			EntryID:                 seed2.EntryID,
+			MatchWeekNumber:         9999,
+			TeamRankings:            []domain.TeamRanking{{Position: 9999}},
+			LegacyEntryPredictionID: seed2.LegacyEntryPredictionID,
+		}
+
+		if err := repo.Update(ctx, changedSeed1); err != nil {
+			t.Fatal(err)
+		}
+
+		want := changedSeed1
+		want.CreatedAt = seed1.CreatedAt // should not be overridden on update
+		want.UpdatedAt = &updatedAt      // should be overridden on update
+
+		got, err := repo.GetByID(ctx, seed1.ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		cmpDiff(t, "match week submission", want, got)
+	})
+
+	t.Run("updating match week submission that does not exist must return the expected error", func(t *testing.T) {
+		repo, err := mysqldb.NewMatchWeekSubmissionRepo(db, nil, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		submission := &domain.MatchWeekSubmission{ID: parseUUID(t, uuidAll3s)}
+
+		wantErrType := domain.MissingDBRecordError{}
+		gotErr := repo.Update(ctx, submission)
+		if !errors.As(gotErr, &wantErrType) {
+			t.Fatalf("want error (%T), got (%T)", wantErrType, gotErr)
+		}
+	})
 }
 
 func cmpDiff(t *testing.T, description string, want, got interface{}) {
@@ -185,6 +350,15 @@ func cmpDiff(t *testing.T, description string, want, got interface{}) {
 	if diff := cmp.Diff(want, got); diff != "" {
 		t.Fatalf("mismatch %s (-want, +got): %s", description, diff)
 	}
+}
+
+func cmpErrorMsg(t *testing.T, wantMsg string, got error) {
+	t.Helper()
+
+	if got == nil {
+		t.Fatalf("want error msg '%s', got nil", wantMsg)
+	}
+	cmpDiff(t, "error msg", wantMsg, got.Error())
 }
 
 func generateMatchWeekSubmission(t *testing.T, id uuid.UUID, createdAt time.Time) *domain.MatchWeekSubmission {
@@ -223,9 +397,18 @@ func seedMatchWeekSubmission(t *testing.T, seed *domain.MatchWeekSubmission) *do
 	return seed
 }
 
+func cloneMatchWeekSubmission(original *domain.MatchWeekSubmission) *domain.MatchWeekSubmission {
+	clone := *original
+	return &clone
+}
+
 func generateEntry() *domain.Entry {
+	id := uuid.New()
+
 	return &domain.Entry{
-		ID: uuid.New(),
+		ID:              id,
+		EntrantNickname: fmt.Sprintf("%s_nickname", id),
+		EntrantEmail:    fmt.Sprintf("%s@seeder.com", id),
 	}
 }
 
