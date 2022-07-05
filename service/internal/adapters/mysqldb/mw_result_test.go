546dd342234809717b567b2b6ff561e09abfcac0
Adam Scholey <hello@adamjs.net>
2022-07-05 21:43:30 +0100

    test: add mw result repo constructor tests

--- service/internal/adapters/mysqldb/mw_result_test.go
+++ service/internal/adapters/mysqldb/mw_result_test.go
@@ -1,10 +1,24 @@
 package mysqldb_test
 
-import "testing"
+import (
+	"errors"
+	"prediction-league/service/internal/adapters/mysqldb"
+	"prediction-league/service/internal/domain"
+	"testing"
+)
 
 func TestNewMatchWeekResultRepo(t *testing.T) {
-	t.Skip()
-	// TODO: feat - write repo constructor tests
+	t.Run("passing non-nil db must succeed", func(t *testing.T) {
+		if _, err := mysqldb.NewMatchWeekResultRepo(db, nil, nil); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("passing nil db must produce the expected error", func(t *testing.T) {
+		if _, err := mysqldb.NewMatchWeekResultRepo(nil, nil, nil); !errors.Is(err, domain.ErrIsNil) {
+			t.Fatalf("want ErrIsNil, got %+v (%T)", err, err)
+		}
+	})
 }
 
 func TestMatchWeekResultRepo_GetBySubmissionID(t *testing.T) {
