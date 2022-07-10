a1f311c2198ed2a900a5fa7e1e370d217eed99f7
Adam Scholey <hello@adamjs.net>
2022-07-10 14:15:23 +0100

    refactor: change layout of controls on registration form

--- resources/src/js/components/registration/RegistrationEntryComponent.vue
+++ resources/src/js/components/registration/RegistrationEntryComponent.vue
@@ -28,68 +28,80 @@
             </div>
         </transition>
         <form id="registration-entry-form" class="form-primary">
-            <div class="row">
-                <div class="col-lg-6 col-md-10">
+          <div class="row">
+            <div class="col-lg-6 col-md-10">
+              <div class="row">
 
-                    <div class="form-label-definition">
-                        <p class="form-label-definition-name">
-                            Nickname
-                            <a data-toggle="collapse" href="#nickname-description" role="button" aria-expanded="false" aria-controls="nickname-description">
-                                <i class="fa fa-question-circle-o" aria-hidden="true"></i>
-                            </a></p>
-                        <p class="collapse form-label-definition-description" id="nickname-description">Displayed publicly. This is what you'll be known as on the leaderboard.</p>
-                    </div>
-
-                    <div class="form-label-group">
-                        <input v-model="formData.entrant_nickname" type="text" id="inputNickname" class="form-control" placeholder="Nickname" required>
-                        <label for="inputNickname">Nickname</label>
-                    </div>
-
+                <div class="col-lg-12">
+                  <div class="form-label-definition">
+                    <p class="form-label-definition-name">
+                      Email
+                      <a class="registration-form-toggle" data-toggle="collapse" href="#email-description" role="button" aria-expanded="false" aria-controls="email-description">
+                        <i class="fa-solid fa-circle-question"></i>
+                      </a></p>
+                    <p class="collapse form-label-definition-description" id="email-description">
+                      The email address we will use to send you game-related communications such as your Short Code
+                      for making a prediction, as well as round-by-round progress updates.
+                      We will ONLY contact you regarding your participation within the Prediction League game.</p>
+                  </div>
+                  <div class="form-label-group">
+                    <input v-model="formData.entrant_email" type="email" id="inputEmail" name="email" class="form-control" placeholder="Email" required>
+                    <label for="inputEmail">Email</label>
+                  </div>
                 </div>
-                <div class="col-lg-6 col-md-10">
 
-                    <div class="form-label-definition">
-                        <p class="form-label-definition-name">
-                            Email
-                            <a data-toggle="collapse" href="#email-description" role="button" aria-expanded="false" aria-controls="email-description">
-                                <i class="fa fa-question-circle-o" aria-hidden="true"></i>
-                            </a></p>
-                        <p class="collapse form-label-definition-description" id="email-description">
-                            The email address we will use to send you game-related communications such as your Short Code
-                            for making a prediction, as well as round-by-round progress updates.
-                            We will ONLY contact you about your direct progress within the Prediction League game.</p>
-                    </div>
+                <div class="col-lg-12">
+                  <div class="form-label-definition">
+                    <p class="form-label-definition-name">
+                      Name
+                      <a class="registration-form-toggle" data-toggle="collapse" href="#name-description" role="button" aria-expanded="false" aria-controls="name-description">
+                        <i class="fa-solid fa-circle-question"></i>
+                      </a></p>
+                    <p class="collapse form-label-definition-description" id="name-description">The name we will use in emails we send to you. This will <strong>not</strong> be publicly displayed.</p>
+                  </div>
 
-                    <div class="form-label-group">
-                        <input v-model="formData.entrant_email" type="email" id="inputEmail" name="email" class="form-control" placeholder="Email" required>
-                        <label for="inputEmail">Email</label>
-                    </div>
-
-                    <div class="form-label-definition">
-                        <p class="form-label-definition-name">
-                            Name
-                            <a data-toggle="collapse" href="#name-description" role="button" aria-expanded="false" aria-controls="name-description">
-                                <i class="fa fa-question-circle-o" aria-hidden="true"></i>
-                            </a></p>
-                        <p class="collapse form-label-definition-description" id="name-description">The name we will use in emails we send to you. This will <strong>not</strong> be publicly displayed.</p>
-                    </div>
-
-                    <div class="form-label-group">
-                        <input v-model="formData.entrant_name" type="text" id="inputName" name="name" class="form-control" placeholder="Name" required>
-                        <label for="inputName">Name</label>
-                    </div>
+                  <div class="form-label-group">
+                    <input v-model="formData.entrant_name" type="text" id="inputName" name="name" class="form-control" placeholder="Name" required>
+                    <label for="inputName">Name</label>
+                  </div>
                 </div>
-                <div class="col-lg-12 col-md-10">
-                    <div class="submit-wrapper">
-                        <action-button
-                                label="Join"
-                                @clicked="enterOnClick"
-                                :is-disabled="working"
-                                :is-working="working"
-                                :is-primary="true"></action-button>
-                    </div>
-                </div>
+              </div>
+
             </div>
+
+            <div class="col-lg-6 col-md-10">
+              <div class="row">
+
+                <div class="col-lg-12">
+                  <div class="form-label-definition">
+                    <p class="form-label-definition-name">
+                      Nickname
+                      <a class="registration-form-toggle" data-toggle="collapse" href="#nickname-description" role="button" aria-expanded="false" aria-controls="nickname-description">
+                        <i class="fa-solid fa-circle-question"></i>
+                      </a></p>
+                    <p class="collapse form-label-definition-description" id="nickname-description">Displayed publicly. This is what you'll be known as on the leaderboard.</p>
+                  </div>
+                  <div class="form-label-group">
+                    <input v-model="formData.entrant_nickname" type="text" id="inputNickname" class="form-control" placeholder="Nickname" required>
+                    <label for="inputNickname">Nickname</label>
+                  </div>
+                </div>
+
+                <div class="col-lg-12">
+                  <div class="submit-wrapper">
+                    <action-button
+                        label="Join"
+                        @clicked="enterOnClick"
+                        :is-disabled="working"
+                        :is-working="working"
+                        :is-primary="true"></action-button>
+                  </div>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
         </form>
     </div>
 </template>
